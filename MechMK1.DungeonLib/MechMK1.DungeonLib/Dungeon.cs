45edd543465ca6901ea59f580ca5fa6915e8a6f1
MechMK1 <dave.stockinger@gmail.com>
2015-01-23 23:04:41 +0100

    Silly reversion madness. Bug fixed.

--- MechMK1.DungeonLib/MechMK1.DungeonLib/Dungeon.cs
+++ MechMK1.DungeonLib/MechMK1.DungeonLib/Dungeon.cs
@@ -69,7 +69,7 @@ namespace MechMK1.DungeonLib
 			}
 		}
 
-		
+
 
 		/// <summary>
 		/// Recursively process all tiles.
@@ -83,85 +83,20 @@ namespace MechMK1.DungeonLib
 			{
 				if ((data & 1) == 1)
 				{
-					Tile adj;
+
 					switch ((Doors)mask)
 					{
 						case Doors.Up:
-							if (t.Y == 0)
-							{
-								t.Doors &= ~Doors.Up; //Remove up since we are already on the top
-								break;
-							}
-
-							if ((adj = Tiles[t.X, t.Y - 1]) == null)
-							{
-								adj = new Tile();
-								adj.Doors |= Doors.Down;
-								SetTile(adj, t.X, t.Y - 1);
-								ProcessTile(adj);
-							}
-							else
-							{
-								adj.Doors |= Doors.Down;
-							}
+							ProcessDoor((Doors)mask, () => (t.Y == 0), Tuple.Create<int, int>(t.X, t.Y - 1), t);
 							break;
-
 						case Doors.Right:
-							if (t.X == Tiles.GetLength(0) - 1)
-							{
-								t.Doors &= ~Doors.Right; //Remove up since we are already on the top
-								break;
-							}
-
-							if ((adj = Tiles[t.X + 1, t.Y]) == null)
-							{
-								adj = new Tile();
-								adj.Doors |= Doors.Left;
-								SetTile(adj, t.X + 1, t.Y);
-								ProcessTile(adj);
-							}
-							else
-							{
-								adj.Doors |= Doors.Left;
-							}
+							ProcessDoor((Doors)mask, () => (t.X == Tiles.GetLength(0) - 1), Tuple.Create<int, int>(t.X + 1, t.Y), t);
 							break;
 						case Doors.Down:
-							if (t.Y == Tiles.GetLength(1) - 1)
-							{
-								t.Doors &= ~Doors.Down; //Remove up since we are already on the top
-								break;
-							}
-
-							if ((adj = Tiles[t.X, t.Y + 1]) == null)
-							{
-								adj = new Tile();
-								adj.Doors |= Doors.Up;
-								SetTile(adj, t.X, t.Y + 1);
-								ProcessTile(adj);
-							}
-							else
-							{
-								adj.Doors |= Doors.Up;
-							}
+							ProcessDoor((Doors)mask, () => (t.Y == Tiles.GetLength(1) - 1), Tuple.Create<int, int>(t.X, t.Y + 1), t);
 							break;
 						case Doors.Left:
-							if (t.X == 0)
-							{
-								t.Doors &= ~Doors.Left; //Remove up since we are already on the top
-								break;
-							}
-
-							if ((adj = Tiles[t.X - 1, t.Y]) == null)
-							{
-								adj = new Tile();
-								adj.Doors |= Doors.Right;
-								SetTile(adj, t.X - 1, t.Y);
-								ProcessTile(adj);
-							}
-							else
-							{
-								adj.Doors |= Doors.Right;
-							}
+							ProcessDoor((Doors)mask, () => (t.X == 0), Tuple.Create<int, int>(t.X - 1, t.Y), t);
 							break;
 						default:
 							throw new ArgumentException("I kinda fucked up here");
@@ -170,6 +105,28 @@ namespace MechMK1.DungeonLib
 
 				mask <<= 1;
 				data >>= 1;
+			}
+		}
+
+		private void ProcessDoor(Doors door, Func<bool> endReached, Tuple<int, int> coords, Tile t)
+		{
+			Tile adj;
+			if (endReached())
+			{
+				t.Doors &= ~door; //Remove up since we are already on the top
+				return;
+			}
+
+			if ((adj = Tiles[coords.Item1, coords.Item2]) == null)
+			{
+				adj = new Tile();
+				adj.Doors |= Util.GetOpposite(door);
+				SetTile(adj, coords.Item1, coords.Item2);
+				ProcessTile(adj);
+			}
+			else
+			{
+				adj.Doors |= Util.GetOpposite(door);
 			}
 		}
 		private void SetTile(Tile tile, int x, int y)
