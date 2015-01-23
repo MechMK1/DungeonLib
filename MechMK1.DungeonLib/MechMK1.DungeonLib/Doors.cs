43117bd11f4e8c60c2b9c1dfbff324c4ee8b296c
MechMK1 <dave.stockinger@gmail.com>
2015-01-23 22:07:18 +0100

    Initial upload

---
+++ MechMK1.DungeonLib/MechMK1.DungeonLib/Doors.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MechMK1.DungeonLib
+{
+	[Flags]
+	public enum Doors : byte
+	{
+		Up = 1,
+		Right = 2,
+		Down = 4,
+		Left = 8
+	}
+}
---
+++ MechMK1.DungeonLib/MechMK1.DungeonLib/Tile.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MechMK1.DungeonLib
+{
+	public class Tile
+	{
+		private static Random r = new Random();
+		private static char[] syms = new char[]
+		{
+			'o',
+			'o',
+			'└',
+			'o',
+			'│',
+			'┌',
+			'├',
+			'o',
+			'┘',
+			'─',
+			'┴',
+			'┐',
+			'┤',
+			'┬',
+			'┼'
+		};
+		public Tile()
+		{
+			this.Doors = GetRandomDoors();
+			this.Symbol = '#';
+		}
+
+		public int Level { get; set; }
+		public char Symbol { get; set; }
+		private Doors doors;
+
+		public Doors Doors
+		{
+			get { return doors; }
+			set { doors = value; UpdateSymbols(); }
+		}
+
+		private void UpdateSymbols()
+		{
+			if (this.Symbol != 'S' && this.Symbol != 'E')
+			{
+				this.Symbol = syms[((byte)this.Doors) - 1];
+			}
+		}
+
+		public int X { get; set; }
+		public int Y { get; set; }
+
+		public int NumberOfDoors()
+		{
+			int value = (byte)this.Doors;
+			int count = 0;
+			while (value != 0)
+			{
+				count++;
+				value &= value - 1;
+			}
+			return count;
+		}
+
+		public bool IsEndTile()
+		{
+			return this.NumberOfDoors() == 1;
+		}
+
+		private Doors GetRandomDoors()
+		{
+			return (Doors)r.Next(1, 16);
+		}
+	}
+}
